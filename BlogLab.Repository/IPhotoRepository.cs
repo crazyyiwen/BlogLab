d67c2fb733b8b8d9ea8f87d1c8bbd71e941130ac
Crazyyiwen <crazyyiwen@gmail.com>
2021-02-07 15:03:30 -0500

    Repository created

---
+++ BlogLab.Repository/IPhotoRepository.cs
@@ -0,0 +1,20 @@
+﻿using BlogLab.Models.Photo;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogLab.Repository {
+    public interface IPhotoRepository {
+
+        public Task<Photo> InsertAsync(PhotoCreate photoCreate, int applicationUserId);
+
+        public Task<Photo> GetAsync ( int photoId );
+
+        public Task<List<Photo>> GetAllByUserAsync ( int applicationUserId );
+
+        public Task<int> DeleteAsync ( int photoId );
+
+    }
+}
