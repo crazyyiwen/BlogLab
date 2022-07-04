4955af4f1eb13a9d33c3916dfad2bf80e2ea0df2
crazyyiwen <crazyyiwen2015@gmail.com>
2022-07-04 00:43:30 -0400

    Model project complete. Repository project created with AccountRepository

---
+++ BlogLab.Repository/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using BlogLab.Models.Account;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace BlogLab.Repository {
+    public interface IAccountRepository {
+
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken);
+        public Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUsername, CancellationToken cancellationToken);
+    }
+}
