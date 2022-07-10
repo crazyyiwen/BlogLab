61bb4b50bc813c789fdc0d89cb32c3be79fd8138
crazyyiwen <crazyyiwen2015@gmail.com>
2022-07-09 21:50:48 -0400

    create components

---
+++ BlogLab-UI/src/app/components/blog-components/famous-blog/famous-blog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-famous-blog',
+  templateUrl: './famous-blog.component.html',
+  styleUrls: ['./famous-blog.component.scss']
+})
+export class FamousBlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
